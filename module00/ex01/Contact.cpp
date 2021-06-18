7bc7abbd393bf9e0b2a5e7d4727363b368b117b1
Jimmie Alease <jalease@student.21-school.ru>
2021-06-19 01:51:08 +0300

    feat: added getters/setters to Contact class

--- module00/ex01/Contact.cpp
+++ module00/ex01/Contact.cpp
@@ -3,3 +3,101 @@
 //
 
 #include "Contact.h"
+
+Contact::Contact()
+: firstName(),
+  lastName(),
+  nickname(),
+  login(),
+  postalAddress(),
+  email(),
+  phone(),
+  birthday(),
+  favoriteMeal(),
+  underwearColor(),
+  secret()
+{}
+
+void Contact::setFirstName(string fistName) {
+	this->firstName = fistName;
+}
+
+void Contact::setLastName(string lastName) {
+	this->lastName = lastName;
+}
+
+void Contact::setNickname(string nickName) {
+	this->nickname = nickName;
+}
+
+void Contact::setLogin(string login) {
+	this->login = login;
+}
+
+void Contact::setPostalAddress(string postalAddress) {
+	this->postalAddress = postalAddress;
+}
+
+void Contact::setEmail(string email) {
+	this->email = email;
+}
+
+void Contact::setPhone(string phone) {
+	this->phone = phone;
+}
+
+void Contact::setFavoriteMeal(string favoriteMeal) {
+	this->favoriteMeal = favoriteMeal;
+}
+
+void Contact::setUnderwearColor(string underwearColor) {
+	this->underwearColor = underwearColor;
+}
+
+void Contact::setSecret(string secret) {
+	this->secret = secret;
+}
+
+string Contact::getFirstName() {
+	return this->firstName;
+}
+
+string Contact::getLastName() {
+	return this->lastName;
+}
+
+string Contact::getNickname() {
+	return this->nickname;
+}
+
+string Contact::getLogin() {
+	return this->login;
+}
+
+string Contact::getPostalAddress() {
+	return this->postalAddress;
+}
+
+string Contact::getEmail() {
+	return this->email;
+}
+
+string Contact::getPhone() {
+	return this->phone;
+}
+
+string Contact::getBirthday() {
+	return this->birthday;
+}
+
+string Contact::getFavoriteMeal() {
+	return this->favoriteMeal;
+}
+
+string Contact::getUnderwearColor() {
+	return this->underwearColor;
+}
+
+string Contact::getSecret() {
+	return this->secret;
+}
--- module00/ex01/Contact.h
+++ module00/ex01/Contact.h
@@ -4,12 +4,48 @@
 
 #ifndef MODULE00_CONTACT_H
 #define MODULE00_CONTACT_H
+#include<iostream>
 
+using namespace std;
 
-class Contact
-{
+class Contact {
+protected:
+	string firstName;
+	string lastName;
+	string nickname;
+	string login;
+	string postalAddress;
+	string email;
+	string phone;
+	string birthday;
+	string favoriteMeal;
+	string underwearColor;
+	string secret;
+public:
+	Contact();
+	void setFirstName(string fistName = "");
+	void setLastName(string lastName = "");
+	void setNickname(string nickName = "");
+	void setLogin(string login = "");
+	void setPostalAddress(string postalAddress = "");
+	void setEmail(string email = "");
+	void setPhone(string phone = "");
+	void setBirthday(string birthday = "");
+	void setFavoriteMeal(string favoriteMeal = "");
+	void setUnderwearColor(string underwearColor = "");
+	void setSecret(string secret = "");
 
+	string getFirstName();
+	string getLastName();
+	string getNickname();
+	string getLogin();
+	string getPostalAddress();
+	string getEmail();
+	string getPhone();
+	string getBirthday();
+	string getFavoriteMeal();
+	string getUnderwearColor();
+	string getSecret();
 };
-
 
 #endif //MODULE00_CONTACT_H
