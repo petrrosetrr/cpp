1e2817d21e9cb117d55a449fcf0011bba6a8a691
Jimmie Alease <jalease@student.21-school.ru>
2021-06-27 11:06:44 +0300

    feat: created files for m03/ex02

---
+++ module03/ex02/FragTrap.h
@@ -0,0 +1,14 @@
+//
+// Created by petr on 27.06.2021.
+//
+
+#ifndef MODULE03_FRAGTRAP_H
+#define MODULE03_FRAGTRAP_H
+
+
+class FragTrap {
+
+};
+
+
+#endif //MODULE03_FRAGTRAP_H
---
+++ module03/ex02/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by petr on 27.06.2021.
+//
+#include <iostream>
+#include "ScavTrap.h"
+
+int main() {
+//	ClapTrap clapTrap("clapClapTrap");
+//	clapTrap.attack("someone");
+//	clapTrap.beRepaired(5);
+//	clapTrap.takeDamage(5);
+//	ClapTrap(clapTrap).takeDamage(10);
+
+	ScavTrap scavTrap("sTrap");
+	std::cout << std::endl;
+	ScavTrap kek(scavTrap);
+	std::cout << std::endl;
+	kek.guardGate();
+	kek.attack("sTrap");
+	std::cout << std::endl;
+}
